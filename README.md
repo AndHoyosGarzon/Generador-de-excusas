9166c92e94ff95145889c33d61551e5c98991e50
andresH <andreshg9203@gmail.com>
2024-04-25 19:55:46 -0400

    feat: creat generator excuses

---
+++ README.md
@@ -0,0 +1,7 @@
+# GENERADOR DE EXCUSAS CREADO CON VANILLA JS
+
+* Se trabajo con las siguientes tecnologias:
+
+-hmtl5
+-Bootstrap Css
+-JavaScript
---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+window.onload = function (){
+
+    const idTitle = document.getElementById('title');
+    idTitle.innerText = 'hola buenas noches'
+
+}
