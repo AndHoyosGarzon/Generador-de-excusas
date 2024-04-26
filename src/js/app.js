eafc34330ff17df68bb2c434f1a7bd3b45ef0519
andresH <andreshg9203@gmail.com>
2024-04-25 20:27:24 -0400

    feat: excuse generator finished

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,19 @@
 window.onload = function (){
+     
+     document.querySelector('#excuses').innerHTML = generadorExcusas();
+}
 
-    const idTitle = document.getElementById('title');
-    idTitle.innerText = 'hola buenas noches'
+let generadorExcusas = () => {
+    
+    let quienFue = ['El perro', 'Mi abuela', 'El cartero', 'Mi pájaro'];
+    let queIso = ['comió', 'pisó', 'aplastó', 'rompió'];
+    let queDaño = ['mi tarea', 'mi teléfono', 'el auto'];
+    let cuandoFue = ['antes de la clase', 'cuando dormía', 'mientras hacía ejercicio', 'durante el almuerzo', 'mientras oraba'];
 
+    let quienIdx = Math.floor(Math.random() * quienFue.length);
+    let isoIdx = Math.floor(Math.random() * queIso.length);
+    let dañoIdx = Math.floor(Math.random() * queDaño.length);
+    let fueIdx = Math.floor(Math.random() * cuandoFue.length);
+
+    return `${quienFue[quienIdx]}, ${queIso[isoIdx]}, ${queDaño[dañoIdx]}, ${cuandoFue[fueIdx]}.`;
 }
